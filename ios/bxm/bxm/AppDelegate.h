d64d2fc6216cc19abf0941224d017b17524a5cce
Sumit Chaudhari <sumit@cryptorai.com>
2018-10-03 21:48:13 +0800

    react router navigation added

---
+++ ios/bxm/bxm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bxm
+//
+//  Created by Sumit C on 03/10/18.
+//  Copyright © 2018 Sumit C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/bxm/bxm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bxm
+//
+//  Created by Sumit C on 03/10/18.
+//  Copyright © 2018 Sumit C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
